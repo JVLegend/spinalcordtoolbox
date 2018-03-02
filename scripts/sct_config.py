d8765fae046f1119b77ef91dbf7641588c6a4aa4
Pierre-Olivier Quirion <pioliqui@gmail.com>
2018-03-02 14:13:23 -0500

    fix data

--- scripts/sct_config.py
+++ scripts/sct_config.py
@@ -53,7 +53,7 @@ def _git_info(commit_env='SCT_COMMIT',branch_env='SCT_BRANCH'):
 
 # Basic sct config
 __sct_dir__ = os.getenv("SCT_DIR", os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-__data_dir__ = os.getenv("SCT_DATA_DIR", os.path.join(__sct_dir__, data))
+__data_dir__ = os.getenv("SCT_DATA_DIR", os.path.join(__sct_dir__, 'data'))
 __version__ = '-'.join(_git_info(commit_env='SCT_COMMIT', branch_env='SCT_BRANCH'))
 
 
